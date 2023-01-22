f10cfeb8d8227e7ec5aa592c832ec219ad42d673
viiprogrammer <m_telega@mail.ru>
2023-01-22 20:25:10 +0300

    chore(views): use windicss

--- src/renderer/views/AboutView.vue
+++ src/renderer/views/AboutView.vue
@@ -1,37 +1,21 @@
 <template>
-  <div class="container">
-    <div class="info">
-      <div class="sakura-logo sakura-color">
-        桜
-      </div>
-      <h1>Sakura</h1>
-      <p>
-        <b>Версия:</b> {{ meta.version }} (64-разрядный)
-      </p>
-      <p>
-        <b>Разработчик:</b> {{ meta.author.name }}
-      </p>
+  <div class="absolute text-center transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+    <div class="sakura-logo sakura-color">
+      桜
     </div>
+    <h1 class="font-bold text-2xl">
+      Sakura
+    </h1>
+    <n-divider />
+    <p>
+      <b>Версия:</b> {{ meta.version }} (64-разрядный)
+    </p>
+    <p>
+      <b>Разработчик:</b> {{ meta.author.name }}
+    </p>
   </div>
 </template>
 
 <script setup>
 import meta from '../../../package.json'
 </script>
-
-<style scoped>
-.container {
-  user-select: none;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100px;
-}
-.info {
-  left: 50%;
-  position: absolute;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-}
-</style>
--- src/renderer/views/HomeView.vue
+++ src/renderer/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="p-4">
     <last-releases />
     <news />
   </div>
@@ -9,10 +9,3 @@
 import LastReleases from '../components/home/LastReleases.vue'
 import News from '../components/home/HomeNews.vue'
 </script>
-
-<style scoped>
-.container {
-  margin-top: 15px;
-  padding: 0 19px 19px;
-}
-</style>
