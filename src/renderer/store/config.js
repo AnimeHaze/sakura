90be19e53f9692e26b25a31342c38c77c6069472
viiprogrammer <m_telega@mail.ru>
2023-01-29 17:13:02 +0300

    chore: rewrite pinia stores with composition API

--- src/renderer/store/config.js
+++ src/renderer/store/config.js
@@ -1,34 +1,33 @@
 import { defineStore } from 'pinia'
+import { computed, ref } from 'vue'
 
-export const useConfigStore = defineStore('config', {
-  state: () => ({
-    /** @type {'light'|'dark'|'auto'} */
-    themeColor: 'auto',
-    /** @type {'light'|'dark'} **/
-    systemThemeColor: 'dark',
-    /** @type {boolean} **/
-    sidebarCollapsed: true,
-    /** @type {'0'|'1'} **/
-    backButtonType: '0'
-  }),
-  getters: {
-    theme () {
-      return this.themeColor === 'auto' ? this.systemThemeColor : this.themeColor
-    }
-  },
-  actions: {
-    collapseSidebar () {
-      this.sidebarCollapsed = true
-    },
-    expandSidebar () {
-      this.sidebarCollapsed = false
-    },
-    toggleTheme () {
-      this.themeColor = this.themeColor === 'dark' ? 'light' : 'dark'
-    },
-    /** @param theme {'light'|'dark'|'auto'} **/
-    setTheme (theme) {
-      this.themeColor = theme
-    }
+export const useConfigStore = defineStore('config', () => {
+  /** @type {Ref<'light'|'dark'|'auto'>} */
+  const themeColor = ref('auto')
+  /** @type {Ref<'light'|'dark'>} */
+  const systemThemeColor = ref('dark')
+  /** @type {Ref<boolean>} */
+  const sidebarCollapsed = ref(true)
+  /** @type {Ref<'0'|'1'>} */
+  const backButtonType = ref('0')
+
+  /** @type {Ref<'light'|'dark'>} */
+  const theme = computed(() => themeColor.value === 'auto' ? systemThemeColor.value : themeColor.value)
+
+  const collapseSidebar = () => (sidebarCollapsed.value = true)
+  const expandSidebar = () => (sidebarCollapsed.value = false)
+  const toggleTheme = () => (themeColor.value = themeColor.value === 'dark' ? 'light' : 'dark')
+  const setTheme = (theme) => (themeColor.value = theme)
+
+  return {
+    themeColor,
+    systemThemeColor,
+    sidebarCollapsed,
+    backButtonType,
+    theme,
+    collapseSidebar,
+    expandSidebar,
+    toggleTheme,
+    setTheme
   }
 })
--- src/renderer/store/user.js
+++ src/renderer/store/user.js
@@ -1,44 +1,39 @@
 import { defineStore } from 'pinia'
+import { computed, ref } from 'vue'
 
-export const useUserStore = defineStore('user', {
-  state: () => ({
-    /** @type {
-     {
-       authToken: {
-         authToken: string|null
-       },
-       name: string,
-       id: number,
-       comments: number,
-       likes: number,
-       friends: number,
-       views: number,
-       avatar: string
-     }
-    } */
-    authToken: null,
-    information: {
-      id: 0,
-      avatar: 'https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg',
-      name: '',
-      comments: 0,
-      friends: 0,
-      likes: 0,
-      views: 0
-    }
-  }),
-  getters: {
-    isAuthorized () {
-      return this.authToken !== null
-    }
-  },
-  actions: {
-    // any amount of arguments, return a promise or not
-    incrementComments (count = 1) {
-      this.information.comments += count
-    },
-    logout () {
-      this.authToken = null
-    }
+/**
+ @typedef Information {Ref<Object>}
+ @property {string} name
+ @property {number} id
+ @property {number} comments
+ @property {number} likes
+ @property {number} friends
+ @property {number} views
+ @property {string} avatar
+ */
+
+export const useUserStore = defineStore('user', () => {
+  /** @type {Ref<string|null>} **/
+  const authToken = ref(null)
+  const information = ref({
+    id: 0,
+    avatar: 'https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg',
+    name: '',
+    comments: 0,
+    friends: 0,
+    likes: 0,
+    views: 0
+  })
+
+  const isAuthorized = computed(() => authToken.value !== null)
+  const incrementComments = (count = 1) => (information.value.comments += count)
+  const logout = () => (authToken.value = null)
+
+  return {
+    isAuthorized,
+    information,
+    authToken,
+    incrementComments,
+    logout
   }
 })
