f20e50a6862f0be1c97f782d98c85194fdb26091
viiprogrammer <m_telega@mail.ru>
2023-09-19 01:24:56 +0300

    feat: add dummy episodes for release page

--- src/renderer/components/release/ReleaseInfo.vue
+++ src/renderer/components/release/ReleaseInfo.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { Star, Heart, Eye, Bookmarks } from '@vicons/ionicons5'
-import { ref } from 'vue'
+import { Star, Heart, Eye, Bookmarks, SwapVerticalOutline } from '@vicons/ionicons5'
 import ReleaseSider from '../../components/release/ReleaseSider.vue'
+import { NIcon } from 'naive-ui'
+import { computed, ref } from 'vue'
 
 defineProps({
   release: {
@@ -10,44 +11,21 @@ defineProps({
   }
 })
 
-const data = Array.from({ length: 46 }).map((_, index) => ({
-  name: `Edward King ${index}`,
-  number: 32,
-  date: `London, Park Lane no. ${index}`,
-  key: index
-}))
+const sort = { ASC: 0, DESC: 1 }
 
-const checkedRowKeys = ref([4, 1])
+const sortDeg = ref(0)
+const sortType = ref(sort.ASC)
+const seriesData = Array.from({ length: 12 })
+  .map((x, index) => ({ number: ++index, id: index }))
 
-const pagination = {
-  pageSize: 6
+const series = computed(() => {
+  return sortType.value === sort.ASC ? [...seriesData].reverse() : seriesData
+})
+
+function swapSort () {
+  sortDeg.value += 360
+  sortType.value = sortType.value === sort.ASC ? sort.DESC : sort.ASC
 }
-
-const createColumns = () => {
-  return [
-    {
-      type: 'selection',
-      multiple: false,
-      disabled (row) {
-        return row.name === 'Edward King 3'
-      }
-    },
-    {
-      title: 'Номер серии',
-      key: 'number'
-    },
-    {
-      title: 'Название',
-      key: 'name'
-    },
-    {
-      title: 'Дата выхода',
-      key: 'date'
-    }
-  ]
-}
-
-const columns = createColumns()
 </script>
 
 <template>
@@ -57,11 +35,11 @@ const columns = createColumns()
       vertical
     >
       <n-layout
-        class="p-6 h-screen"
+        class="h-screen"
+        position="absolute"
         has-sider
       >
         <n-layout-sider
-          :native-scrollbar="false"
           content-style="padding: 24px;"
         >
           <release-sider
@@ -71,7 +49,7 @@ const columns = createColumns()
         </n-layout-sider>
         <n-layout-content
           :native-scrollbar="false"
-          class="p-4 py-2"
+          content-style="padding: 24px;"
         >
           <n-space
             vertical
@@ -151,13 +129,85 @@ const columns = createColumns()
             >
               {{ release.description }}
 
-              <div class="py-3">
-                <n-data-table
-                  v-model:checked-row-keys="checkedRowKeys"
-                  :columns="columns"
-                  :data="data"
-                  :pagination="pagination"
-                />
+              <div class="py-1">
+                <div>
+                  <n-divider class="n-divider" />
+                  <div class="flex">
+                    <div class="flex-none">
+                      <n-button
+                        strong
+                        secondary
+                        @click="swapSort()"
+                      >
+                        <n-icon
+                          size="24"
+                          class="transition"
+                          :style="{ transform: `rotate(${sortDeg}deg)` }"
+                        >
+                          <SwapVerticalOutline />
+                        </n-icon>
+                      </n-button>
+                    </div>
+
+                    <n-input
+                      class="mb-2 ml-2 w-full"
+                      type="text"
+                      placeholder="Поиск по сериям"
+                      loading
+                    />
+                  </div>
+
+                  <div
+                    v-for="serie in series"
+                    :key="serie.id"
+                    class="w-full mb-2 rounded-md p-2 episode cursor-pointer"
+                  >
+                    <n-space justify="space-between">
+                      <div
+                        class="inline-flex gap-1"
+                      >
+                        <n-space
+                          class="ml-1 mt-1"
+                          justify="start"
+                          style="width: 115px;"
+                        >
+                          <n-tooltip
+                            placement="right"
+                            trigger="hover"
+                          >
+                            <template #trigger>
+                              <n-icon
+                                size="20"
+                              >
+                                <eye />
+                              </n-icon>
+                            </template>
+                            Не просмотренно
+                          </n-tooltip>
+
+                          <div class="font-bold">
+                            Серия {{ serie.number }}
+                          </div>
+                        </n-space>
+
+                        <n-space>
+                          <n-tag
+                            v-for="resolution in [1080, 720, 480]"
+                            :key="resolution"
+                            :bordered="false"
+                            :type="resolution === 1080 ? 'success' : 'default'"
+                          >
+                            {{ resolution }}p
+                          </n-tag>
+                        </n-space>
+                      </div>
+
+                      <div class="mt-2 mr-2 text-zinc-500">
+                        01.01.2023
+                      </div>
+                    </n-space>
+                  </div>
+                </div>
               </div>
             </n-tab-pane>
             <n-tab-pane
@@ -174,5 +224,20 @@ const columns = createColumns()
 </template>
 
 <style scoped>
+.episode {
+  background: rgb(102 102 102 / 18%);
+}
 
+.episode:hover {
+  background: rgb(102 102 102 / 13%);
+}
+
+.transition {
+  transition: transform 0.5s ease-in-out;
+}
+
+.n-divider:not(.n-divider--vertical) {
+  margin-top: 8px;
+  margin-bottom: 13px;
+}
 </style>
