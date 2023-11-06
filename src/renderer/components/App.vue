f0bc6a469f8cce69e982d3536a35e705d5bb3164
viiprogrammer <m_telega@mail.ru>
2023-11-07 02:34:28 +0300

    feat: allow to hide / show memory usage from context menu

--- src/renderer/components/App.vue
+++ src/renderer/components/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <div style="position: absolute;top: 18px;right: 21px;width: 200px;background: #70766dad;z-index: 9999999999999;color: #f9f9f9;padding: 10px;border-radius: 5px;">
+  <div
+    v-show="showMemoryUsage"
+    style="position: absolute;top: 18px;right: 21px;width: 200px;background: #70766dad;z-index: 9999999999999;color: #f9f9f9;padding: 10px;border-radius: 5px;"
+  >
     <div>Total heap size: {{ totalJSHeapSize }} MB</div>
     <div>Used heap size: {{ usedJSHeapSize }} MB</div>
     <div>Heap size limit: {{ jsHeapSizeLimit }} MB</div>
@@ -26,12 +29,22 @@
           :y="y"
           trigger="manual"
         >
-          <n-button
-            size="small"
-            @click="inspectElement()"
-          >
-            Исследовать
-          </n-button>
+          <div>
+            <n-button
+              size="small"
+              @click="inspectElement()"
+            >
+              Исследовать
+            </n-button>
+          </div>
+          <div class="mt-3">
+            <n-button
+              size="small"
+              @click="inspectMemoryUsage()"
+            >
+              {{ showMemoryUsage ? 'Скрыть использование памяти' : 'Показать использование памяти' }}
+            </n-button>
+          </div>
         </n-popover>
       </n-message-provider>
     </n-dialog-provider>
@@ -39,28 +52,20 @@
 </template>
 
 <script setup>
-import { computed, onBeforeUnmount, onMounted, ref } from 'vue'
+import { computed, onBeforeUnmount, onMounted, ref, watch } from 'vue'
 import { darkTheme, lightTheme } from 'naive-ui'
 import { useConfigStore } from '../store'
 import { appTheme } from '../../enums'
 
+const showMemoryUsage = ref(false)
 const totalJSHeapSize = ref(0)
 const usedJSHeapSize = ref(0)
 const jsHeapSizeLimit = ref(0)
 const mainJsRSS = ref(0)
 
-const memoryUsageInterval = setInterval(async () => {
-  const { totalJSHeapSize: totalJSHeapSizeTmp, usedJSHeapSize: usedJSHeapSizeTmp, jsHeapSizeLimit: jsHeapSizeLimitTmp } = performance.memory
-  totalJSHeapSize.value = (totalJSHeapSizeTmp / (1024 * 1024)).toFixed(2)
-  usedJSHeapSize.value = (usedJSHeapSizeTmp / (1024 * 1024)).toFixed(2)
-  jsHeapSizeLimit.value = (jsHeapSizeLimitTmp / (1024 * 1024)).toFixed(2)
-
-  const { rss } = await window.api.memoryUsage()
-  mainJsRSS.value = (rss / (1024 * 1024)).toFixed(2)
-}, 300)
+let memoryUsageInterval = null
 
 const config = useConfigStore()
-
 const theme = computed(() => config.theme === appTheme.DARK ? darkTheme : lightTheme)
 
 function themeChange (event) {
@@ -76,6 +81,11 @@ function inspectElement () {
   window.api.inspectElement(x.value, y.value)
 }
 
+function inspectMemoryUsage () {
+  showPopover.value = false
+  showMemoryUsage.value = !showMemoryUsage.value
+}
+
 function handleContextMenu (event) {
   if (showPopover.value) {
     showPopover.value = true
@@ -88,16 +98,35 @@ function handleContextMenu (event) {
   }
 }
 
-const mediaDark = window.matchMedia('(prefers-color-scheme: dark)')
-themeChange(mediaDark)
+function startMemoryPoll () {
+  return setInterval(async () => {
+    const { totalJSHeapSize: totalJSHeapSizeTmp, usedJSHeapSize: usedJSHeapSizeTmp, jsHeapSizeLimit: jsHeapSizeLimitTmp } = performance.memory
+    totalJSHeapSize.value = (totalJSHeapSizeTmp / (1024 * 1024)).toFixed(2)
+    usedJSHeapSize.value = (usedJSHeapSizeTmp / (1024 * 1024)).toFixed(2)
+    jsHeapSizeLimit.value = (jsHeapSizeLimitTmp / (1024 * 1024)).toFixed(2)
 
-mediaDark.addEventListener('change', themeChange)
+    const { rss } = await window.api.memoryUsage()
+    mainJsRSS.value = (rss / (1024 * 1024)).toFixed(2)
+  }, 300)
+}
+
+const mediaDark = window.matchMedia('(prefers-color-scheme: dark)')
+
+watch(() => showMemoryUsage.value, (value) => {
+  if (value) {
+    memoryUsageInterval = startMemoryPoll()
+  } else {
+    clearInterval(memoryUsageInterval)
+  }
+}, { immediate: true })
+
+onMounted(() => {
+  themeChange(mediaDark)
+  mediaDark.addEventListener('change', themeChange)
+})
 
 onBeforeUnmount(() => {
   mediaDark.removeEventListener('change', themeChange)
-})
-
-onMounted(() => {
   clearInterval(memoryUsageInterval)
 })
 </script>
