9eeec59a4fa7ee41d296debb7be8de5e7bab479b
viiprogrammer <m_telega@mail.ru>
2023-11-07 03:40:37 +0300

    chore: fix release skeleton paddings

--- src/renderer/components/release/ReleaseInfoSkeleton.vue
+++ src/renderer/components/release/ReleaseInfoSkeleton.vue
@@ -22,122 +22,124 @@ import ReleaseEpisodesSkeleton from './ReleaseEpisodesSkeleton.vue'
         </n-layout-sider>
         <n-layout-content
           :native-scrollbar="false"
-          style="padding: 24px;"
+          content-style="padding: 0 0 24px 24px;"
         >
-          <n-space
-            vertical
-          >
-            <div class="flex-1">
-              <n-skeleton
-                :height="32"
-                :width="806"
-                :sharp="false"
-                size="medium"
-              />
-            </div>
-            <div>
-              <n-skeleton
-                :height="21"
-                :width="300"
-                :sharp="false"
-                size="medium"
-              />
-            </div>
-          </n-space>
+          <div class="mt-6 mr-5">
+            <n-space
+              vertical
+            >
+              <div class="flex-1">
+                <n-skeleton
+                  :height="32"
+                  width="100%"
+                  :sharp="false"
+                  size="medium"
+                />
+              </div>
+              <div>
+                <n-skeleton
+                  :height="21"
+                  :width="300"
+                  :sharp="false"
+                  size="medium"
+                />
+              </div>
+            </n-space>
 
-          <n-space
-            size="small"
-            class="mt-3"
-          >
-            <n-skeleton
-              v-for="genre in 5"
-              :key="genre"
-              :height="28"
-              :width="60 + (genre * 5)"
-              :sharp="false"
-              size="medium"
-            />
-          </n-space>
-
-          <n-space class="mt-3">
-            <n-button>
-              <n-icon>
-                <star style="color: #ffa726;" />
-              </n-icon>
-              <n-skeleton
-                class="ml-2"
-                :height="14"
-                :width="35"
-                :sharp="false"
-                size="medium"
-              />
-            </n-button>
-
-            <n-button>
-              <n-icon>
-                <eye />
-              </n-icon>
-              <n-skeleton
-                class="ml-2"
-                :height="14"
-                :width="35"
-                :sharp="false"
-                size="medium"
-              />
-            </n-button>
-
-            <n-button>
-              <n-icon>
-                <heart />
-              </n-icon>
-              <n-skeleton
-                class="ml-2"
-                :height="14"
-                :width="35"
-                :sharp="false"
-                size="medium"
-              />
-            </n-button>
-
-            <n-button>
-              <n-icon>
-                <bookmarks />
-              </n-icon>
-              <n-skeleton
-                class="ml-2"
-                :height="14"
-                :width="35"
-                :sharp="false"
-                size="medium"
-              />
-            </n-button>
-          </n-space>
-
-          <n-tabs
-            class="mt-2"
-            type="line"
-            animated
-          >
-            <n-tab-pane
-              name="information"
-              tab="Информация"
+            <n-space
+              size="small"
+              class="mt-3"
             >
               <n-skeleton
-                :height="120"
-                width="100%"
+                v-for="genre in 5"
+                :key="genre"
+                :height="28"
+                :width="60 + (genre * 5)"
                 :sharp="false"
                 size="medium"
               />
+            </n-space>
 
-              <div class="py-1">
-                <div>
-                  <n-divider class="n-divider" />
+            <n-space class="mt-3">
+              <n-button>
+                <n-icon>
+                  <star style="color: #ffa726;" />
+                </n-icon>
+                <n-skeleton
+                  class="ml-2"
+                  :height="14"
+                  :width="35"
+                  :sharp="false"
+                  size="medium"
+                />
+              </n-button>
 
-                  <release-episodes-skeleton />
+              <n-button>
+                <n-icon>
+                  <eye />
+                </n-icon>
+                <n-skeleton
+                  class="ml-2"
+                  :height="14"
+                  :width="35"
+                  :sharp="false"
+                  size="medium"
+                />
+              </n-button>
+
+              <n-button>
+                <n-icon>
+                  <heart />
+                </n-icon>
+                <n-skeleton
+                  class="ml-2"
+                  :height="14"
+                  :width="35"
+                  :sharp="false"
+                  size="medium"
+                />
+              </n-button>
+
+              <n-button>
+                <n-icon>
+                  <bookmarks />
+                </n-icon>
+                <n-skeleton
+                  class="ml-2"
+                  :height="14"
+                  :width="35"
+                  :sharp="false"
+                  size="medium"
+                />
+              </n-button>
+            </n-space>
+
+            <n-tabs
+              class="mt-2"
+              type="line"
+              animated
+            >
+              <n-tab-pane
+                name="information"
+                tab="Информация"
+              >
+                <n-skeleton
+                  :height="120"
+                  width="100%"
+                  :sharp="false"
+                  size="medium"
+                />
+
+                <div class="py-1">
+                  <div>
+                    <n-divider class="n-divider" />
+
+                    <release-episodes-skeleton />
+                  </div>
                 </div>
-              </div>
-            </n-tab-pane>
-          </n-tabs>
+              </n-tab-pane>
+            </n-tabs>
+          </div>
         </n-layout-content>
       </n-layout>
     </n-space>
