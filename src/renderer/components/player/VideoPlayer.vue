9c65fbdfc8edbea8b30dced7b49cbee25e938ee0
viiprogrammer <m_telega@mail.ru>
2023-11-18 01:53:37 +0300

    fix: video poster looks cropped before video loaded

--- src/renderer/components/player/VideoPlayer.vue
+++ src/renderer/components/player/VideoPlayer.vue
@@ -62,7 +62,8 @@ defineEmits(['openEpisodes', 'openRelease'])
       @provider-change="onProviderChange"
       @can-play="onCanPlay"
     >
-      <media-provider>
+      <!-- h-full fixes "cropped poster line" when video not loaded https://i.imgur.com/2EscXVK.png -->
+      <media-provider class="h-full">
         <media-poster
           class="absolute inset-0 block h-full w-full rounded-md opacity-0 transition-opacity data-[visible]:opacity-100 [&>img]:h-full [&>img]:w-full [&>img]:object-cover"
           src="https://image.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/thumbnail.webp?time=268&width=1200"
@@ -72,9 +73,9 @@ defineEmits(['openEpisodes', 'openRelease'])
 
       <VideoLayout
         :poster="poster"
+        thumbnails="https://image.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/storyboard.vtt"
         @open-release="$emit('openRelease')"
         @open-episodes="$emit('openEpisodes')"
-        thumbnails="https://image.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/storyboard.vtt"
       />
     </media-player>
   </div>
