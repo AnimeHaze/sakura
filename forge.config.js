f6a72b236f8db4bc6d7ab886ebf9745fb8f38e6b
viiprogrammer <m_telega@mail.ru>
2023-11-18 02:27:28 +0300

    chore: setup app makers

--- forge.config.js
+++ forge.config.js
@@ -1,22 +1,93 @@
+const path = require('node:path')
+const meta = require('./package.json')
+
 module.exports = {
-  packagerConfig: {},
+  packagerConfig: {
+    executableName: meta.productName,
+    icon: path.resolve(__dirname, 'assets', 'logo', 'logo')
+  },
   rebuildConfig: {},
   makers: [
+    {
+      name: '@electron-forge/maker-deb',
+      config: {
+        options: {
+          bin: meta.productName, // https://github.com/electron-userland/electron-installer-debian/issues/175#issuecomment-1558131497,
+          name: meta.productName,
+          homepage: meta.homepage,
+          maintainer: meta.author.name,
+          description: meta.description,
+          section: 'video',
+          categories: ['AudioVideo'],
+          icon: path.resolve(__dirname, 'assets', 'logo', 'logo.png')
+        }
+      }
+    },
     {
       name: '@electron-forge/maker-squirrel',
       config: {}
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin']
+      platforms: ['darwin', 'linux', 'windows']
     },
     {
-      name: '@electron-forge/maker-deb',
-      config: {}
+      name: '@electron-forge/maker-snap',
+      config: {
+        icon: path.resolve(__dirname, 'assets', 'logo', 'logo.png'),
+        features: {
+          audio: true,
+          webgl: true
+        },
+        version: meta.version,
+        base: 'core18',
+        grade: 'stable',
+        issues: meta.bugs.url,
+        summary: meta.description,
+        description: meta.description
+      }
     },
     {
       name: '@electron-forge/maker-rpm',
+      config: {
+        categories: ['AudioVideo'],
+        compressionLevel: 9
+      }
+    },
+    {
+      name: '@electron-forge/maker-dmg',
+      config: {
+        overwrite: true,
+        iconSize: 1024,
+        format: 'ULFO'
+      }
+    },
+    {
+      name: '@electron-forge/maker-flatpak',
+      config: {
+        options: {
+          categories: ['AudioVideo'],
+          id: 'org.animehaze.sakura'
+        }
+      }
+    },
+    {
+      name: '@electron-forge/maker-pkg',
       config: {}
+    },
+    {
+      name: '@reforged/maker-appimage',
+      config: {
+        options: {
+          name: meta.productName,
+          bin: meta.productName,
+          productName: meta.productName,
+          genericName: meta.productName,
+          icon: path.resolve(__dirname, 'assets', 'logo', 'logo.png'),
+          AppImageKitRelease: 'continuous',
+          flagsFile: 'true'
+        }
+      }
     }
   ],
   plugins: [
